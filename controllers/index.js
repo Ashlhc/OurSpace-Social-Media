de8ca9bb51ddd03c7a4843d6eeff380d299c061b
Alec Hunter <alecryanhunter@gmail.com>
2023-05-08 14:18:22 -0700

    added test login route

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,8 @@ const apiRoutes = require("./api");
 const searchRoutes = require("./searchController");
 const signupRoutes = require("./signupController");
 
+// THESE ROUTES ARE ALL FOR TESTING PURPOSES
+
 router.get("/",(req,res)=>{
     res.send("You found the home page!");
 })
@@ -11,6 +13,18 @@ router.get("/",(req,res)=>{
 router.get("/sessiondata",(req,res)=>{
     res.json(req.session);
 })
+
+// To sign up, need:
+// username
+// password
+// first name
+// last name
+router.post("/signup",(req,res)=>{
+    res.json(req.body)
+})
+
+// END TESTING ROUTES
+
 
 router.use("/api",apiRoutes);
 router.use("/search",searchRoutes);
