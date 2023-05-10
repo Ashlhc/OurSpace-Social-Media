c8913f6a4fd7ee457db03323180c25d6a324a4c5
Alec Hunter <alecryanhunter@gmail.com>
2023-05-10 13:08:22 -0700

    merged friend controller with user controller

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// GET ROUTE - ALL
 router.get('/', async (req, res) => {
   try {
     const users = await User.findAll();
@@ -11,11 +12,12 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get("/users",(req,res)=>{
+// GET ROUTE - ALL INCLUDING FRIENDS
+router.get("/friends",(req,res)=>{
   User.findAll({
       include: {
           model: User,
-          as: "friend"
+          as: "Friends"
       }
   })
   .then(allUsers=>{
@@ -27,6 +29,7 @@ router.get("/users",(req,res)=>{
   });
 })
 
+// GET ROUTE - SINGULAR BY ID
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
@@ -41,16 +44,42 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// GET ROUTE - SINGULAR BY ID INCLUDING FRIENDS
+router.get("/:userId/friends", (req,res)=>{
+  User.findAll({
+      where: { id: req.params.userId},
+      include: {
+          model: User,
+          as: "Friends",
+          attributes: ["id", "username", "first_name", "last_name"],
+      },
+  })
+  .then((user)=>{
+      if(!user) {
+          return res.status(404).json({error: err});
+      }
+      res.json(user.friends);
+  })
+  .catch((err)=> {
+      console.log(err);
+      res.status(500).json({error: err});
+  });
+});
+
+// POST ROUTE
+// JSON FORMAT
+// username
+// first_name
+// last_name
+// password
 router.post('/', async (req, res) => {
-  const { username, first_name, last_name, password, profile_img, bio } = req.body;
+  const { username, first_name, last_name, password} = req.body;
   try {
     const newUser = await User.create({
       username,
       first_name,
       last_name,
-      password,
-      profile_img,
-      bio
+      password
     });
     res.status(201).json(newUser);
   } catch (err) {
@@ -59,20 +88,35 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.post("/addFriend",(req,res)=>{
-  User.addFriend({
-      UserId: req.body.user_id,
-      FriendId: req.body.friend_id
+// POST ROUTE - ADD FRIEND
+// userId is the person clicking add friend
+// friendId is the person they're clicking on
+router.post("/:userId/newfriend/:friendId", (req,res)=> {
+  User.findByPk(req.params.userId)
+  .then((user)=> {
+      if(!user) {
+          return res.status(404).json({error: err});
+      }
+      User.findByPk(req.params.friendId)
+      .then((friends)=> {
+          if (!friends) {
+              return res.status(404).json({error: err});
+          }
+          user.addFriend(friends);
+          res.json({msg:"Friend added!"});
+      })
+      .catch((err)=> {
+          console.log(err);
+          res.status(500).json({error: err});
+      });
   })
-  .then(addFriend=>{
-      res.json(addFriend)
-  })
-  .catch(err=>{
+  .catch((err)=> {
       console.log(err);
       res.status(500).json({error: err});
   });
-})
+});
 
+// UPDATE ROUTE
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const { username, first_name, last_name, password, profile_img, bio } = req.body;
@@ -96,7 +140,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-
+// DELETE ROUTE
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   try {
@@ -110,6 +154,28 @@ router.delete('/:id', async (req, res) => {
     console.error(err);
     res.status(500).json({error: err});
   }
+});
+
+// DELETE ROUTE - FRIEND BY ID
+router.delete("/:userId/friends/:friendId", (req,res)=> {
+  User.findByPk(req.params.userId)
+  .then((user)=> {
+      if(!user) {
+          return res.status(404).json({error: err});
+      }
+      User.findByPk(req.params.friendId)
+      .then((friend)=> {
+          if(!friend) {
+              return res.status(404).json({error: err});
+          }
+          user.removeFriend(friend);
+          res.json({msg:"Friend Removed"});
+      });
+  })
+  .catch((err)=> {
+      console.log(err);
+      res.status(500).json({error: err});
+  });
 });
 
 module.exports = router;
