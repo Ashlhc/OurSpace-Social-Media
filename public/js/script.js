7b05dcb6982582dee879021d68790ef3122094c3
Alec Hunter <alecryanhunter@gmail.com>
2023-05-10 16:54:59 -0700

    added if, blocking blank searches

--- public/js/script.js
+++ public/js/script.js
@@ -3,9 +3,11 @@ const search = document.querySelector("#search");
 
 search.addEventListener("submit",function(event) {
     event.preventDefault();
-
-    const userSearch = document.querySelector("#search input").value;
-    window.location.href = `/search/${userSearch}`
+    
+    const userSearch = document.querySelector("#search input").value.trim();
+    if (userSearch!=="") {
+        window.location.href = `/search/${userSearch}`
+    }
 });
 
 // Logout
