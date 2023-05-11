3f34af97426329d9d43b595f888c2dbcaf398ed7
Alec Hunter <alecryanhunter@gmail.com>
2023-05-11 10:58:53 -0700

    fixed signup duplicate username bug

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,17 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const htmlRoutes = require('./htmlController');
 
+// For retrieving session data for user in front-end
 router.get("/sessiondata",(req,res)=>{
     res.json(req.session);
 })
 
 router.use("/api",apiRoutes);
 router.use(htmlRoutes);
+
+// Universal redirect to home page
+router.use("*",(req,res)=>{
+    res.redirect("/");
+})
 
 module.exports = router;
