88b38c6d806a2e67600ea755e36d7cd444c59c87
Erick-McGee <erickmcgee3@gmail.com>
2023-05-11 18:12:37 -0700

    sizing refinement for profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -66,7 +66,7 @@ async function editHandler() {
 
         // Switches button to save mode
         editBtn.textContent = "Save";
-        editBtn.classList.add
+        editBtn.classList.add("save")
         editBtn.id = 'save';
     }
 }
